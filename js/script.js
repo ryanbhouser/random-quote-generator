662f27e4388a7681ec76632e1f350cfc8a82efac
Ryan Houser <r.houser90@gmail.com>
2019-04-09 17:25:17 -0400

    Random quote on load & tag styles
    quote appears on page load, styled and placed tag in htmlString

--- js/script.js
+++ js/script.js
@@ -1,73 +1,71 @@
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
 // Quotes array
 var quotes = [
   {
     quote: 'Whatever happens tomorrow you must promise me one thing. That you will stay who you are. Not a perfect soldier, but a good man.',
-    character: 'Dr. Abraham Erskine',
-    source: 'Captain America:  The First Avenger',
+    source: 'Dr. Abraham Erskine',
+    citation: 'Captain America:  The First Avenger',
     year: '2011',
     tag: 'captain america'
   },
   {
     quote: 'A wise man once asked, "Is it better to be feared or respected?" I say, is it too much to ask for both?',
-    character: 'Tony Stark',
-    source:  'Iron Man',
+    source: 'Tony Stark',
+    citation:  'Iron Man',
     year: '2008',
     tag: 'iron man'
   },
   {
     quote: 'You are no match for the mighty Thor!',
-    character: 'Thor',
     source: 'Thor',
+    citation: 'Thor',
     year: '2011',
     tag: 'thor'
   },
   {
     quote: 'You can\'t destroy power. All you can do is to make sure that it\'s in the right hands.',
-    character: 'Dr. Hank Pym',
-    source: 'Ant Man',
+    source: 'Dr. Hank Pym',
+    citation: 'Ant Man',
     year: '2015',
     tag: 'ant man'
   },
   {
     quote: 'Have you ever had someone pick your brain and play? Pull you out, stuff something else in. Do you know what it\s like to be unmade?',
-    character: 'Hawkeye',
-    source: 'The Avengers',
+    source: 'Hawkeye',
+    citation: 'The Avengers',
     year: '2012',
     tag: 'avengers'
   },
   {
     quote: 'I know what it\'s like to lose. To feel so desperately that you\'re right, yet to fail nonetheless.',
-    character: 'Thanos',
-    source: 'Avengers: Infinity War',
+    source: 'Thanos',
+    citation: 'Avengers: Infinity War',
     year: '2018',
     tag: 'avengers'
   },
   {
     quote: 'We\'re in the endgame now.',
-    character: 'Doctor Strange',
-    source: 'Avengers:  Infinity War',
+    source: 'Doctor Strange',
+    citation: 'Avengers:  Infinity War',
     year: '2018',
     tag: 'avengers'
   },
   {
     quote: 'When you can do the things that I can, but you don\'t, and then the bad things happen, they happen because of you.',
-    character: 'Spiderman',
-    source: 'Spiderman:  Homecoming',
+    source: 'Spiderman',
+    citation: 'Spiderman:  Homecoming',
     year: '2017',
     tag: 'spiderman'
   },
   {
     quote: 'This man declined the Nobel Peace Prize. He said peace wasn\'t an achievement, it was a responsibility. See, it\'s stuff like this that gives me trust issues.',
-    character: 'Nick Fury',
-    source: 'Captain America:  The Winter Soldier',
+    source: 'Nick Fury',
+    citation: 'Captain America:  The Winter Soldier',
     year: '2014',
     tag: 'captain america'
   }
 ];
 
-// colors = red, blue, gree, yellow, purple, lt green, orange, dk blue, gray
+// Background colors = red, blue, gree, yellow, purple, lt green, orange, dk blue, gray
 var colors = ['#c0392b', '#2980b9', '#16a085', '#f39c12', '#8e44ad', '#27ae60', '#d35400', '#2c3e50', '#7f8c8d'];
 
 // Create random number and return an object from quotes array
@@ -75,7 +73,6 @@ function getRandomQuote() {
   var generateIndex = Math.floor(Math.random() * quotes.length);
   return quotes[generateIndex];
 }
-
 
 /***
   Create the `printQuote` function to: 
@@ -102,8 +99,13 @@ function printQuote() {
   var quote = getRandomQuote();
   var htmlString = '';
   htmlString += '<p class="quote">' + quote.quote + '</p>';
-  htmlString += '<p class="source">' + quote.character + '<span class="citation">' + quote.source + '</span><span class="year">' + quote.year + '</span></p>';
+  htmlString += '<p class="source">' + quote.source + '<span class="citation">' + quote.citation + '</span><span class="year">' + quote.year + '</span></p>';
+  htmlString += '<p class="tag">Tagged under: ' + quote.tag + '</p>';
   document.getElementById('quote-box').innerHTML = htmlString;   
 }
 
+// Ensures a random quote appears on page load
+printQuote();
+
+// Generates a new random quote and displays on screen
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
